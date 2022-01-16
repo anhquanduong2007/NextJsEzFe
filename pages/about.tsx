a5496efc5a10319107e3baa63f4441b0d9c34e9f
valaqua2007 <quanduongforworks2007@gmail.com>
2022-01-16 13:05:47 +0700

    B6 - Automatic Static Optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,22 @@
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
 export interface AboutPageProps { }
 
 export default function AboutPage(props: AboutPageProps) {
+    const router = useRouter();
+    console.log("About query", router.query);
+    React.useEffect(() => {
+        // check query available 
+    }, [])
     return (
         <div>
             About Page
         </div>
     );
+}
+export function getServerSideProps() {
+    return {
+        props: {}
+    }
 }
